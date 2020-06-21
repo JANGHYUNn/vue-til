312b8ec45233df5bcdf9ad2c8c05bf99474b190f
JANGHYUNn <kimjh2369@gmail.com>
2020-06-22 03:42:36 +0900

    router modify

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,23 +7,23 @@ export default new VueRouter({
   mode: "history",
   routes: [
     {
-      path: "/",
-      redirect: "/login"
+      path: "/vue-til",
+      redirect: "/vue-til/login"
     },
     {
-      path: "/login",
+      path: "/vue-til/login",
       // 다이나믹 임포트
       // 한번에 전체페이지를 로딩하지 않아 초기 로딩속도 줄여들임,
       // /login으로 진입시에만 loginPage를 로딩함
       component: () => import("@/views/LoginPage.vue")
     },
     {
-      path: "/signup",
+      path: "/vue-til/signup",
       component: () => import("@/views/SignupPage.vue")
     },
     {
       // "*": routes설정된 페이지를 제외한 모든 페이지에 대한
-      path: "*",
+      path: "/vue-til/*",
       component: () => import("@/views/NotFoundPage.vue")
     }
   ]
